b446d6bc0722b137f0c17572939951d2103279f6
JibinXu <jibin.xu@mthreads.com>
2025-12-25 10:55:14 +0800

    update mtdma int mask
    Signed-off-by: JibinXu <jibin.xu@mthreads.com>

--- driver/mt-emu-mtdma-bare.c
+++ driver/mt-emu-mtdma-bare.c
@@ -33,7 +33,7 @@ MODULE_LICENSE("GPL v2");
 void mtdma_comm_init(void __iomem * mtdma_comm_vaddr, int vf_num) {
 	int i;
 	u32 ver = 0, c32, c64;
-	u64 dma_mask, val;;
+	u64 dma_mask;
 
 	ver = GET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_BASIC_PARAM);
 	pr_info("mtdma version is: 0x%x\n", ver);
@@ -46,13 +46,12 @@ void mtdma_comm_init(void __iomem * mtdma_comm_vaddr, int vf_num) {
 	SET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_MST0_BLEN, (MST0_ARLEN<<4) | MST0_AWLEN);
 	SET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_MST1_BLEN, (MST1_ARLEN<<4) | MST1_AWLEN);
 
-
 	for (i = 0; i < vf_num; i++) {
 		SET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_CH_OSID(i), 0x1);
 		SET_COMM_32(mtdma_comm_vaddr + 0x2000, 0x4 * i, i);
 	}
 
-	dma_mask = (val << vf_num) | ((1ull << vf_num) - 1);
+	dma_mask = ((1ull << vf_num) - 1);
 	c32 = dma_mask & 0xffffffff;
 	c64 = dma_mask >> 32;
 
@@ -64,10 +63,8 @@ void mtdma_comm_init(void __iomem * mtdma_comm_vaddr, int vf_num) {
 	SET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_WR_MRG_PF0_IMSK_C32, c32);
 	SET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_WR_MRG_PF0_IMSK_C64, c64);
 
-	if (GET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_WORK_STS)) {
+	if (GET_COMM_32(mtdma_comm_vaddr, REG_DMA_COMM_WORK_STS))
 		printk( "dma init with busy error\n");
-	}
-
 }
 
 void build_dma_info(void *mtdma_vaddr, uint64_t mtdma_paddr, void __iomem *rg_vaddr, void __iomem *ll_vaddr, u8 vf, u8 wr_ch_cnt, u8 rd_ch_cnt, struct mtdma_info *dma_info)
